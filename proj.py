039e15ce5f264a01f1bc73270f1ed0e505d75596
sanosenx86 <44931458+sanosenx86@users.noreply.github.com>
2020-11-24 07:26:33 +0800

    Update whatever :( Add pipeline, add skeleton

--- proj.py
+++ proj.py
@@ -1,17 +1,37 @@
 import sys
 import cv2
 
-## To execute, do python proj.py <input1> <input2>.
-## Will write to out.jpg
+# To execute, do python proj.py <input1> <input2>.
+# Will write to out.jpg
 
-## TODO: descriptor
-## TODO: matching
-## TODO: stitching
+# Pipeline: find interest points, then create SIFT descriptor, then matching them, then find stitching line.
 
-## Basic entry point with I/O operation.
+
+# TODO: descriptor
+"""
+    returns a list of points which are considered as features.
+"""
+
+
+def sift(img):
+    pass
+
+
+# TODO: matching
+
+def match_imgs(img1, img2):
+    pass
+
+
+# TODO: stitching
+def stitch_imgs(img1, img2, line):
+    pass
+
+
+# Basic entry point with I/O operation.
 
 if __name__ == '__main__':
     img1 = cv2.imread(sys.argv[1])
     img2 = cv2.imread(sys.argv[2])
-    result = None ## TODO: complete code.
+    result = None  ## TODO: complete code.
     cv2.imwrite("out.jpg", result)
