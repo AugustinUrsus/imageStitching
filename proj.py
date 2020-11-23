3502ee40c18a71bbd64a96fe3cc5daa39849d760
sanosenx86 <44931458+sanosenx86@users.noreply.github.com>
2020-11-24 07:35:50 +0800

    minor update: copied from homework 8 as a starting point.

--- proj.py
+++ proj.py
@@ -1,5 +1,8 @@
+import math
 import sys
 import cv2
+import numpy as np
+
 
 # To execute, do python proj.py <input1> <input2>.
 # Will write to out.jpg
@@ -7,11 +10,42 @@ import cv2
 # Pipeline: find interest points, then create SIFT descriptor, then matching them, then find stitching line.
 
 
-# TODO: descriptor
-"""
-    returns a list of points which are considered as features.
-"""
+# TODO: interest points: change to RGB detector.
 
+def gaussDeriv2D(sigma):
+    """ Generate 2d gaussian filter given sigma (sigma > 0) of size 3*sigma
+        @Output: 2d gaussian filter as 2d array
+    """
+    filter_size = 2 * math.ceil(3 * sigma) + 1
+    g_x = math.ceil(3 * sigma) - np.ones((filter_size, filter_size)) * np.array(range(filter_size))
+    g_y = -g_x.transpose()
+
+    dividend = 2 * math.pi * sigma ** 4
+
+    G_x = -np.multiply(g_x / dividend, np.exp(-(np.square(g_x) + np.square(g_y)) / (2 * sigma * sigma)))
+    G_y = -G_x.transpose()
+
+    return [G_x, G_y]
+
+
+def harris(img, si=1, sd=0.7, R=range(16, 23), T=1e6, a=0.05):
+    size_i = int(math.ceil(si * 3) * 2 + 1)
+    weight = cv2.getGaussianKernel(ksize=size_i, sigma=si)
+    weight = weight.dot(weight.T).view('float64')
+
+    gx, gy = gaussDeriv2D(sd)
+    dr = cv2.filter2D(img, ddepth=cv2.CV_64F, kernel=gx).view('float64')
+    dc = cv2.filter2D(img, ddepth=cv2.CV_64F, kernel=gy).view('float64')
+
+    h = np.stack((dr * dr, dr * dc, dc * dc), axis=-1).view('float64')
+
+    M = cv2.filter2D(h, ddepth=cv2.CV_64F, kernel=weight)
+
+    r_score = (M[:, :, 0] * M[:, :, 2] - M[:, :, 1] * M[:, :, 1]) - a * np.square(M[:, :, 0] + M[:, :, 2])
+    return r_score
+
+
+# TODO: descriptor
 
 def sift(img):
     pass
@@ -33,5 +67,5 @@ def stitch_imgs(img1, img2, line):
 if __name__ == '__main__':
     img1 = cv2.imread(sys.argv[1])
     img2 = cv2.imread(sys.argv[2])
-    result = None  ## TODO: complete code.
+    result = None  # TODO: complete code.
     cv2.imwrite("out.jpg", result)
